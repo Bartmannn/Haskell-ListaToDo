a96e6593f87a8f1fd955f5eb705c2165e777350c
Bartosz Bohdziewicz <bohdziewicz.bartosz@gmail.com>
2025-05-29 17:56:20 +0200

    Dodawanie tasków

--- src/Task.hs
+++ src/Task.hs
@@ -1,6 +1,6 @@
 module Task where
 
-import Date (DueDate)
+import Date (DueDate, parseDueDate)
 
 data Priority = Low | Medium | High deriving (Show, Eq)
 
@@ -13,15 +13,14 @@ data Task = Task
     , done          :: Bool
     } deriving (Show, Eq)
 
-addTask :: [Task] -> String -> String -> DueDate -> Priority -> [Task]
-addTask tasks title ""
-addTask tasks title description date priority =
-    Task (nextId tasks) title description date priority False : tasks
+addTask :: [Task] -> String -> String -> String -> Priority -> [Task]
+addTask tasks title description dateStr priority =
+    let nextId = if null tasks then 1 else maximum (map tid tasks) + 1
+        Just date = parseDueDate dateStr
+        newTask = Task nextId title description date priority False
+    in newTask : tasks -- TODO: sortowanie
 
 nextId :: [Task] -> Integer
 nextId [] = 1
 nextId ts = maximum (map tid ts) + 1
-
-parseDate :: String -> Maybe Day
-parseDate = parseTimeM True defaultTimeLocale "%Y-%m-%d"
 
