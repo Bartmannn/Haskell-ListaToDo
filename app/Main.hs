de16268f5823dddafee858059e559750a46ccf8e
Weronika <wera.chmiela123@gmail.com>
2025-06-09 20:56:30 +0200

    modify i inne

--- app/Main.hs
+++ app/Main.hs
@@ -52,49 +52,45 @@ helpMenu = do
     putStrLn " 8 / exit / koniec       - wyjdź i zapisz postęp"
 --    putStrLn " 9 / load / zalacz       - załącz inny plik "
 
---TODO wszedzie pokazywac tym ładnym
 choiceMenu :: [Task] -> IO [Task]
 choiceMenu tasks = do
     putStrLn "Wybierz operacje 1-8"
     opt <- getLine
     --TODO *
     case map toLower opt of
-        "1" -> addMenu tasks
-        "add" -> addMenu tasks
-        "dodaj" -> addMenu tasks
-        "2" -> doneMenu tasks
-        "done" -> doneMenu tasks
-        "ukoncz" -> doneMenu tasks
-        "3" -> modifyMenu tasks --TODO
-        "modify" -> modifyMenu tasks 
-        "edit" -> modifyMenu tasks 
-        "4" -> deleteMenu tasks
-        "delete" -> deleteMenu tasks
-        "usun" -> deleteMenu tasks
-        "5" -> printMenu tasks
-        "print" -> printMenu tasks 
-        "show" -> printMenu tasks 
-        "wyswietl" -> printMenu tasks 
-        "6" -> sortMenu tasks 
-        "sort" -> sortMenu tasks
-        "filtr" -> sortMenu tasks
-        "6 id" -> sortId tasks
-        "sortid" -> sortId tasks
-        "6 idr" -> sortIdR tasks
-        "sortidr" -> sortIdR tasks
-        "sortdt" -> sortDate tasks
-        "sortdtr" ->  sortDateR tasks
-        "sortpr"-> sortPrior tasks
-        "sortprr"-> sortPriorR tasks
-        "7" -> do
-            helpMenu
-            choiceMenu tasks
-        "help" -> do
-            helpMenu
-            choiceMenu tasks 
-        "pomoc" -> do
-            helpMenu
-            choiceMenu tasks
+        "1" -> addMenu tasks >>= choiceMenu
+        "add" -> addMenu tasks >>= choiceMenu
+        "dodaj" -> addMenu tasks >>= choiceMenu
+        "2" -> doneMenu tasks >>= choiceMenu
+        "done" -> doneMenu tasks >>= choiceMenu
+        "ukoncz" -> doneMenu tasks >>= choiceMenu
+        "3" -> modifyMenu tasks >>= choiceMenu
+        "modify" -> modifyMenu tasks >>= choiceMenu
+        "edit" -> modifyMenu tasks >>= choiceMenu
+        "4" -> deleteMenu tasks >>= choiceMenu
+        "delete" -> deleteMenu tasks >>= choiceMenu
+        "usun" -> deleteMenu tasks >>= choiceMenu
+        "5" -> printMenu tasks >>= choiceMenu
+        "print" -> printMenu tasks >>= choiceMenu
+        "show" -> printMenu tasks >>= choiceMenu
+        "wyswietl" -> printMenu tasks >>= choiceMenu
+        "6" -> sortMenu tasks >>= choiceMenu
+        "sort" -> sortMenu tasks >>= choiceMenu
+        "filtr" -> sortMenu tasks >>= choiceMenu
+        "6 id" -> sortId tasks >>= choiceMenu
+        "sortid" -> sortId tasks >>= choiceMenu
+        "6 idr" -> sortIdR tasks >>= choiceMenu
+        "sortidr" -> sortIdR tasks >>= choiceMenu
+        "sortdt" -> sortDate tasks >>= choiceMenu
+        "sortdtr" ->  sortDateR tasks >>= choiceMenu
+        "sortpr"-> sortPrior tasks >>= choiceMenu
+        "sortprr"-> sortPriorR tasks >>= choiceMenu
+        "sortdone" -> sortOnlyDone tasks >>= choiceMenu
+        "sortndone" -> sortOnlyNotDone tasks >>= choiceMenu
+        "7" -> helpMenu >> choiceMenu tasks
+        "help" -> helpMenu >> choiceMenu tasks 
+        "h" -> helpMenu >> choiceMenu tasks 
+        "pomoc" -> helpMenu >> choiceMenu tasks
         "8" -> return tasks
         "exit" -> return tasks
         "q" -> return tasks
@@ -103,9 +99,8 @@ choiceMenu tasks = do
 --        "9" -> putStrLn "Tak" --TODO*
         _ -> do 
             putStrLn "Niepoprawna komenda" >> choiceMenu tasks
-        return tasks --usun
 
-addMenu :: [Task] -> IO ()
+addMenu :: [Task] -> IO [Task]
 addMenu tasks = do
     putStrLn "=== Dodawanie zadania ==="
     putStrLn "Tytuł:"
@@ -120,23 +115,25 @@ addMenu tasks = do
     --Parsujemy priorytet jako wartość typu Priority
     let prio = case map toLower pStr of
                    "high"   -> High
+                   "h"      -> High 
+                   "m"      -> Medium
                    "medium" -> Medium
                    _        -> Low
  
     case addTaskSafe tasks t d dateStr prio of
         Left err -> do
             putStrLn $ "Niedodano zadania - " ++ err
-            choiceMenu tasks --bez zmian
+            return tasks --bez zmian
         Right newTasks -> do 
             putStrLn "\nZadanie dodane:"
-            print (head newTasks) --podgląd 
+            putStrLn $ prettyPrintTask (head newTasks) --TODO bezpieczniej niz head?
             putStrLn "=== ==="
-            choiceMenu newTasks
+            return newTasks
 
-doneMenu :: [Task] -> IO ()
+doneMenu :: [Task] -> IO [Task]
 doneMenu tasks = do
     putStrLn "=== Kończenie zadania ==="
-    putStrLn "Id:"
+    putStrLn "Podaj id zadania:"
     input <- getLine
     case readMaybe input :: Maybe Integer of
         Just taskId -> do
@@ -146,7 +143,7 @@ doneMenu tasks = do
                 then putStrLn "Zadanie o podanym ID nie istnieje."
                 else do
                    putStrLn "\nUkończone zadanie:"
-                   print thisIdTasks
+                   putStrLn $ prettyPrintTasks thisIdTasks 
             putStrLn "=== ==="
             choiceMenu updatedTasks
         Nothing     -> do
@@ -154,14 +151,99 @@ doneMenu tasks = do
             putStrLn "=== ==="
             choiceMenu tasks
 
---TODO
-modifyMenu :: [Task] -> IO ()
+modifyMenu :: [Task] -> IO [Task]
 modifyMenu tasks = do
-     putStrLn "=== Edytowanie zadania ==="
-     putStrLn "TODO Id:"
-     choiceMenu tasks
+    putStrLn "=== Edytowanie zadania ==="
+    putStrLn "Podaj id zadania:"
+    input <- getLine
 
-deleteMenu :: [Task] -> IO ()
+    --zadanie dla podanego id
+    case readMaybe input :: Maybe Integer of
+        Just id -> do
+            let idTasks = filter (\t -> tid t == id) tasks
+            
+            if idTasks == [] 
+                then do
+                    putStrLn "Zadanie o podanym ID nie istnieje."
+                    putStrLn "=== ==="
+                    return tasks
+                else do
+                    putStrLn "Wybrane zadanie:"
+                    putStrLn $ prettyPrintTasks idTasks 
+                    
+                    --wybor zmiany
+                    putStrLn "Co chcesz zmienic w zadaniu: tytuł, opis, date, priorytet"
+                    inputType <- getLine
+                    let lowerInput = map toLower inputType
+                    
+                    if lowerInput `elem` ["tytul","tytuł","title","1", "t"] 
+                        then do 
+                            putStrLn "Podaj nowy tytuł:"
+                            newTitle <- getLine
+
+                            let updatedTasks = modifyTaskById id (\t -> t { title = newTitle }) tasks 
+                            --putStrLn $ prettyPrintTasks updatedTasks
+                            putStrLn "Zmieniono tytuł"
+                            putStrLn $ prettyPrintTasks (filter (\t -> tid t == id) updatedTasks)
+                            putStrLn "=== ==="
+                            return updatedTasks
+                    else if lowerInput `elem` ["opis","description","2", "o"] 
+                        then do
+                            putStrLn "Podaj nowy opis:"
+                            newDes <- getLine
+
+                            let updatedTasks = modifyTaskById id (\t -> t { description = newDes }) tasks 
+                            putStrLn "Zmieniono opis"
+                            putStrLn $ prettyPrintTasks (filter (\t -> tid t == id) updatedTasks)
+                            putStrLn "=== ==="
+                            return updatedTasks
+                    else if lowerInput `elem` ["data","date","finish","3", "d"] 
+                        then do   
+                            putStrLn "Podaj nową date wykonania (DD-MM-YYYY):"
+                            datestr <- getLine     
+                            case parseDueDateSafe datestr of
+                                Left err -> do 
+                                    putStrLn "Podana data jest niepoprawna - niewykonano zmiany"
+                                    putStrLn "=== ==="
+                                    return tasks
+                                Right newDate -> do
+                                    let updatedTasks = modifyTaskById id (\t -> t { dueDate = newDate }) tasks
+                                    putStrLn "Zmieniono date"
+                                    putStrLn $ prettyPrintTasks (filter (\t -> tid t == id) updatedTasks)
+                                    putStrLn "=== ==="
+                                    return updatedTasks 
+                                    
+                    else if lowerInput `elem` ["priorytet","priority","4", "p"] 
+                        then do
+                            putStrLn "Podaj nowy priorytet (Low, Medium, High):"
+                            prior <- getLine
+
+                            let newPrio = case map toLower prior of
+                                  "high"   -> High 
+                                  "h"      -> High
+                                  "medium" -> Medium
+                                  "m"      -> Medium
+                                  _        -> Low
+    
+                            let updatedTasks = modifyTaskById id (\t -> t { priority = newPrio }) tasks
+                            
+                            putStrLn "Zmieniono priorytet"
+                            putStrLn $ prettyPrintTasks (filter (\t -> tid t == id) updatedTasks)
+                            putStrLn "=== ==="
+                            return updatedTasks 
+                        
+                        
+                    else do     
+                        putStrLn "Nie ma takiej opcji"
+                        putStrLn "=== ==="
+                        return tasks
+
+        Nothing -> do
+            putStrLn "Id musi być liczbą całkowitą"
+            putStrLn "=== ==="
+            return tasks
+
+deleteMenu :: [Task] -> IO [Task]
 deleteMenu tasks = do
     putStrLn "=== Usuwanie zadania ==="
     putStrLn "Id:"
@@ -173,34 +255,36 @@ deleteMenu tasks = do
                 then do            
                     putStrLn "Zadanie o podanym ID nie istnieje."
                     putStrLn "=== ==="
-                    choiceMenu tasks
+                    return tasks
                 else do
                    putStrLn "Usuwanie zadania:"
-                   print foundTasks
+                   putStrLn $ prettyPrintTasks foundTasks ----------- 
                    let updatedTasks = removeTaskById taskId tasks --nie przeszkadza jesli nie ma takiego
                    putStrLn "=== ==="
-                   choiceMenu updatedTasks
+                   return updatedTasks
         Nothing     -> do
             putStrLn "Id musi być liczbą całkowitą"
             putStrLn "=== ==="
-            choiceMenu tasks
+            return tasks
 
 
-printMenu :: [Task] -> IO ()
+printMenu :: [Task] -> IO [Task]
 printMenu tasks = do
     putStrLn $ prettyPrintTasks tasks
-    choiceMenu tasks
+    return tasks
 
-sortMenu :: [Task] -> IO ()
+sortMenu :: [Task] -> IO [Task] 
 sortMenu tasks = do  
     putStrLn "=== Sortowanie zadań ==="
     putStrLn "Możliwe sortowanie:"
     putStrLn " Id      - sortuje po Id rosnąco"
     putStrLn " IdR     - sortuje po Id malejąco"
-    putStrLn " Dt    - sortuje po dacie rosnąco"
-    putStrLn " DtR   - sortuje po dacie malejąco"
-    putStrLn " Pr    - sortuje od najsilnijeszego priorytetu"
-    putStrLn " PrR   - sortuje od najsłabszego priorytetu"
+    putStrLn " Dt      - sortuje po dacie rosnąco"
+    putStrLn " DtR     - sortuje po dacie malejąco"
+    putStrLn " Pr      - sortuje od najsilnijeszego priorytetu"
+    putStrLn " PrR     - sortuje od najsłabszego priorytetu"
+    putStrLn " Done    - pokazuje tylko ukończone"
+    putStrLn " NDone   - pokazuje tylko nie ukończone"
     opt <- getLine
     case map toLower opt of
         "id" -> sortId tasks
@@ -215,47 +299,64 @@ sortMenu tasks = do
         "pr dec" -> sortPrior tasks
         "prr" -> sortPriorR tasks
         "pr inc" -> sortPriorR tasks
-        _ -> choiceMenu tasks
-sortId :: [Task] -> IO ()
+        "done" -> sortOnlyDone tasks
+        "ndone" -> sortOnlyNotDone tasks
+        "not done" -> sortOnlyNotDone tasks
+        _ -> return tasks 
+
+sortId :: [Task] -> IO [Task]
 sortId tasks = do
     putStrLn "=== Sortowanie wg Id rosnąco ==="
     let sta = sortById tasks 
     putStrLn $ prettyPrintTasks sta
-    choiceMenu sta
+    return sta
 
-sortIdR :: [Task] -> IO ()
+sortIdR :: [Task] -> IO [Task]
 sortIdR tasks = do    
     putStrLn "=== Sortowanie wg Id - malejąco ==="
     let stasks = sortByIdDesc tasks
     putStrLn $ prettyPrintTasks stasks
-    choiceMenu stasks
+    return stasks
 
-sortDate :: [Task] -> IO ()
+sortDate :: [Task] -> IO [Task]
 sortDate tasks = do
     putStrLn "=== Sortowanie wg Daty - rosnąco  ==="
     let sta = sortByDate tasks
     putStrLn $ prettyPrintTasks sta 
-    choiceMenu sta
+    return sta
 
-sortDateR :: [Task] -> IO ()
+sortDateR :: [Task] -> IO [Task]
 sortDateR tasks = do
     putStrLn "=== Sortowanie wg Daty - malejąco ==="
     let sta = sortByDateDesc tasks
     putStrLn $ prettyPrintTasks sta 
-    choiceMenu sta
+    return sta
 
-sortPrior :: [Task] -> IO ()
+sortPrior :: [Task] -> IO [Task]
 sortPrior tasks = do
     putStrLn "=== Sortowanie wg Priorytetu od najmocniejszego ==="
     let sta = sortByPriority tasks
     putStrLn $ prettyPrintTasks sta 
-    choiceMenu sta
+    return sta
 
-sortPriorR :: [Task] -> IO ()
+sortPriorR :: [Task] -> IO [Task]
 sortPriorR tasks = do
     putStrLn "=== Sortowanie wg Priorytetu od najsłabszego ==="
     let sta = sortByPriorityInc tasks
     putStrLn $ prettyPrintTasks sta 
-    choiceMenu sta
+    return sta
 
+sortOnlyDone :: [Task] -> IO [Task] 
+sortOnlyDone tasks = do
+    putStrLn "=== Wyświetlono tylko ukończone ==="
+    let dtasks = (filter (\t -> done t == True) tasks)
+    putStrLn $ prettyPrintTasks dtasks
+    return tasks  
+
+sortOnlyNotDone :: [Task] -> IO [Task] 
+sortOnlyNotDone tasks = do
+    putStrLn "=== Wyświetlono tylko nie ukończone ==="
+    let dtasks = (filter (\t -> done t == False) tasks)
+    putStrLn $ prettyPrintTasks dtasks
+    return tasks
 
